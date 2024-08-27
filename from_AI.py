65d52e33744be01efbcbea5546e6571003f89c6d
TDS <tds@tcg3pl.com>
2024-08-27 16:16:55 -0400

    Adding HomeGoods Logo.

--- from_AI.py
+++ from_AI.py
@@ -7,7 +7,7 @@ from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl.utils.dataframe import dataframe_to_rows
 from io import BytesIO
 
-st.logo(image, *, link='https://www.homegoods.com/img/header/homegoods-logo.svg', icon_image=None)
+st.logo(image, link='https://www.homegoods.com/img/header/homegoods-logo.svg', icon_image=None)
 st.title("The Home Goods Database App 🗂️")
 
 st.caption("""
